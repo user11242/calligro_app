45af71d8407179d87bb42333779df23bc479c272
Yazan Qattous <yaz20220877@std.psut.edu.jo>
2025-08-26 14:37:39 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sfdsfsd")
+        ),
+        body: const Text("Hello World!"),
+    );
+  }
+}
